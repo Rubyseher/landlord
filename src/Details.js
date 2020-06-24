1347612b24fe4d73a0d63860a7a786cdd7d43dcb
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2020-06-24 20:02:45 +0530

    table

--- src/Details.js
+++ src/Details.js
@@ -9,11 +9,58 @@ class Details extends React.Component {
         return(
     		<div id="container">
     		<h1>{DB[this.props.id].Name}</h1>
-            <p><b>Start Date: </b>{DB[this.props.id]["Start Date"]}</p>
+        <table style="width:100%">
+          <tr>
+          <th>Start Date:</th>
+             <th>RR No:</th>
+             <th>Acc ID:</th>
+             <th>MR Code:</th>
+             <th>ID: </th>
+           </tr>
+           <tr>
+             <td>{DB[this.props.id]["Start Date"]}</td>
+             <td>{DB[this.props.id]["RR No"]}</td>
+             <td>{DB[this.props.id]["Acc ID"]}</td>
+             <td>{DB[this.props.id]["MR Code"]}</td>
+             <td>{DB[this.props.id]["ID"]}</td>
+           </tr>
+          </table>
 
-    		</div>
-    	)
-    }
+          <table>
+          <tr>
+            <th>Paid Rent:</th>
+             <th>Month:</th>
+             <th>Date:</th>
+             <th>Amount:</th>
+          </tr>
+             {   DB[this.props.id]["Paid Rent"].map(p=>
+            <tr>
+              <td>{p.Month}</td>
+              <td>{p.Date}</td>
+              <td>{p.Amount}</td>
+            </tr>
+            )
+            }
+          </table>
+
+          <table>
+          <tr>
+             <th>Renewal</th>
+             <th>Month:</th>
+             <th>Date:</th>
+          </tr>
+             {DB[this.props.id]["Renewal"]?
+                 DB[this.props.id]["Renewal"].map(p=>
+              <tr>
+              <td>{p.Month}</td>
+              <td>{p.Date}</td>
+              </tr>
+            ):null
+              }
+          </table>
+          </div>
+        )
+  }
 }
 
 export default Details;
