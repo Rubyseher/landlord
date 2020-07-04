a964f9dad9a2c186cac9bda8d87324c7dccb8a74
ruby <rubyseher2002@gmail.com>
2020-07-04 13:58:34 +0530

    firebase

--- src/Details.js
+++ src/Details.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import DB from './data.json';
+// import this.state.DB from './data.json';
+import Firebase from 'firebase';
+import config from './config';
 
 class Details extends React.Component {
     constructor(props) {
         super(props)
-        console.log(props);
+        this.state = {
+          DB:null
+        };
+
+        if (!Firebase.apps.length) {
+          Firebase.initializeApp(config);
+          this.getUserData()
+          console.log(this.state.DB);
+        }
     }
+
+    getUserData = () => {
+    let ref = Firebase.database().ref('/');
+    ref.on('value', snapshot => {
+      this.setState({ DB: snapshot.val()});
+  		console.log(snapshot.val());
+    });
+  }
+  componentDidMount() {
+  this.getUserData();
+}
 	render() {
         return(
-    		<div id="container">
-    		<h1>{DB[this.props.location.state.id].Name}</h1>
+    			this.state.DB && <div id="container">
+    		<h1>{this.state.DB[this.props.location.state.id].Name}</h1>
 
-             <p><b>Start Date:</b>{DB[this.props.location.state.id]["Start Date"]}</p>
-             <p><b>RR No:</b>{DB[this.props.location.state.id]["RR No"]}</p>
-             <p><b>Acc ID:</b>{DB[this.props.location.state.id]["Acc ID"]}</p>
-             <p><b>MR Code:</b>{DB[this.props.location.state.id]["MR Code"]}</p>
-             <p><b>ID:</b>{DB[this.props.location.state.id]["ID"]}</p>
+             <p><b>Start Date:</b>{this.state.DB[this.props.location.state.id]["Start Date"]}</p>
+             <p><b>RR No:</b>{this.state.DB[this.props.location.state.id]["RR No"]}</p>
+             <p><b>Acc ID:</b>{this.state.DB[this.props.location.state.id]["Acc ID"]}</p>
+             <p><b>MR Code:</b>{this.state.DB[this.props.location.state.id]["MR Code"]}</p>
+             <p><b>ID:</b>{this.state.DB[this.props.location.state.id]["ID"]}</p>
 
     <h3>Paid Rent:</h3>
     <center>
@@ -26,7 +47,7 @@ class Details extends React.Component {
              <th>Date:</th>
              <th>Amount:</th>
           </tr>
-             {   DB[this.props.location.state.id]["Paid Rent"].map(p=>
+             {   this.state.DB[this.props.location.state.id]["Paid Rent"].map(p=>
             <tr>
               <td>{p.Month}</td>
               <td>{p.Date}</td>
@@ -38,8 +59,8 @@ class Details extends React.Component {
           </center>
 
           <h3>Renewal Date:</h3>
-             {DB[this.props.location.state.id]["Renewal"]?
-                 DB[this.props.location.state.id]["Renewal"].map(p=>
+             {this.state.DB[this.props.location.state.id]["Renewal"]?
+                 this.state.DB[this.props.location.state.id]["Renewal"].map(p=>
               <p>{p.Date}</p>
             ):null
               }
--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import DB from './data.json';
+// import this.state.DB from './data.json';
 import { Redirect, Router } from 'react-router';
 import Details from './Details';
 import './Main.css'
 import moment from 'moment';
+import Firebase from 'firebase';
+import config from './config';
 
 class Main extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			id: null,
-			redirect:null
+			redirect:null,
+			DB:null
+		}
+		if (!Firebase.apps.length) {
+			Firebase.initializeApp(config);
+			this.getUserData()
+			console.log(this.state.DB);
 		}
 	}
+	componentDidMount() {
+  this.getUserData();
+}
 
 	detailsRedirect = (id) => {
 		this.setState({id: id});
@@ -23,22 +34,30 @@ class Main extends React.Component {
 	AddRedirect= () => {
 	this.setState({redirect:"/Add"})
 	}
-	checkRent = (id) => {
-		if(!DB[id]["Paid Rent"])
-			return -1;
-		else if (DB[id]["Paid Rent"].length<1) return -1;
 
-		let paid = DB[id]["Paid Rent"].sort((a,b) => {return a.Month-b.Month})
+	getUserData = () => {
+  let ref = Firebase.database().ref('/');
+  ref.on('value', snapshot => {
+    this.setState({ DB: snapshot.val()});
+		console.log(snapshot.val());
+  });
+}
+	checkRent = (id) => {
+		if(!this.state.DB[id]["Paid Rent"])
+			return -1;
+		else if (this.state.DB[id]["Paid Rent"].length<1) return -1;
+
+		let paid = this.state.DB[id]["Paid Rent"].sort((a,b) => {return a.Month-b.Month})
 		let expected = [], due = [], dueTotal = 0;
 
 		paid.forEach((p,i) => {
-			if(DB[id].Renewal) {
-				if(DB[id].Renewal.length>0) {
+			if(this.state.DB[id].Renewal) {
+				if(this.state.DB[id].Renewal.length>0) {
 					let period = p.Month/11
-					if(period<=1) expected.push(DB[id].Rent);
-					else expected.push(DB[id].Rent*(1.05*Math.floor(period)));
+					if(period<=1) expected.push(this.state.DB[id].Rent);
+					else expected.push(this.state.DB[id].Rent*(1.05*Math.floor(period)));
 				}
-			} else expected.push(DB[id].Rent);
+			} else expected.push(this.state.DB[id].Rent);
 			if(p.Month !== paid[0].Month+i) {
 				let diff = p.Month - paid[i-1].Month - 1;
 
@@ -74,8 +93,8 @@ class Main extends React.Component {
 
 	checkRenewal=(id)=>
 	{
-		let r=DB[id].Renewal
-		let endDate = moment((r!==undefined && r.length>0) ? r[r.length-1]["Date"] : DB[id]["Start Date"],"M/D/YY", true).add(11,"M")
+		let r=this.state.DB[id].Renewal
+		let endDate = moment((r!==undefined && r.length>0) ? r[r.length-1]["Date"] : this.state.DB[id]["Start Date"],"M/D/YY", true).add(11,"M")
 		return endDate.isBetween(moment().subtract(1,"M"),moment().add(4,"M"),"M") ? endDate.format("Do MMMM, YYYY") : null;
 	}
 	render () {
@@ -85,17 +104,17 @@ class Main extends React.Component {
             state: { id: this.state.id }
         }} />
 		return(
-		<div class="container">
+		this.state.DB && <div class="container">
 		<div class="circle" onClick={() => this.AddRedirect()}style={{ backgroundColor: '#0057e0',color:"white"}}>
 		<i class="fa fa-plus" aria-hidden="true"></i></div>
 		<h2>Rent #86: {moment().subtract(1, 'months').format("MMM")} - {moment().format("MMM")}</h2>
 		<div class="nameList">
 		{
-		  Object.keys(DB).map((d,i) => (
+		  Object.keys(this.state.DB).map((d,i) => (
 			  (d[0] === '8') ? <div key={d} class="person" onClick={() => this.detailsRedirect(d)} style={{marginRight: (i%3 === 2) ? "0%" : "5%"}}>
 			   <div class="circle" style={{ backgroundColor: this.rentColor(d)}}><i class={this.rentColor(d,"icon")}></i></div>
 			   <div class="name">
-				   {d.split('_')[1] === '0' ? 'G' : d.split('_')[1]}F {DB[d].Nickname ? DB[d].Nickname : DB[d].Name.split(' ')[0]}
+				   {d.split('_')[1] === '0' ? 'G' : d.split('_')[1]}F {this.state.DB[d].Nickname ? this.state.DB[d].Nickname : this.state.DB[d].Name.split(' ')[0]}
 			   </div>
 			  </div> : null
 	  		))
@@ -104,11 +123,11 @@ class Main extends React.Component {
 		<h2>Rent #6: {moment().subtract(1, 'months').format("MMM")} - {moment().format("MMM")}</h2>
 		<div class="nameList">
 		{
-		  Object.keys(DB).map((d,i) => (
+		  Object.keys(this.state.DB).map((d,i) => (
 			  (d[0] === '6') ? <div key={d} class="person" onClick={() => this.detailsRedirect(d)} style={{marginRight: (i%3 === 2) ? "0%" : "5%"}}>
 			  <div class="circle" style={{ backgroundColor:this.rentColor(d) }}><i class={this.rentColor(d,"icon")}></i></div>
 			   <div class="name">
-				   {d.split('_')[1] === '0' ? 'G' : d.split('_')[1]}F {DB[d].Nickname ? DB[d].Nickname : DB[d].Name.split(' ')[0]}
+				   {d.split('_')[1] === '0' ? 'G' : d.split('_')[1]}F {this.state.DB[d].Nickname ? this.state.DB[d].Nickname : this.state.DB[d].Name.split(' ')[0]}
 			   </div>
 			  </div> : null
 	  		))
@@ -117,9 +136,9 @@ class Main extends React.Component {
 		<h2>Upcoming Renewals</h2>
 		<div class="nameList" style={{display: "block", alignItems: "left"}}>
 		{
-			Object.keys(DB).map(d => (
+			Object.keys(this.state.DB).map(d => (
 				this.checkRenewal(d)?
-				<p key={d}><b>{DB[d].Name}:</b> {this.checkRenewal(d)}<br/></p>:null
+				<p key={d}><b>{this.state.DB[d].Name}:</b> {this.checkRenewal(d)}<br/></p>:null
 			))
 		}
 		</div>
